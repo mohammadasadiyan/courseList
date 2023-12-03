793d585e2dde02c73ab3c845898d38940146c327
soulle3s <mmdbro7@gmail.com>
2023-12-03 18:18:08 +0330

    course list version 1.0 with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [course, setCourse] = useState([]);
+  const [newCourse, setNewCourse] = useState("");
+  const handleChange = (event)=> {
+    setNewCourse(event.target.value)
+  }
+  const appendToList = ()=> {
+    const newList = [...course, newCourse]
+    setCourse(newList)
+  }
+  const deleteCourse = (courseName)=> {
+    const del = course.filter((course)=> {
+      if (course === courseName) return false
+      else return true
+    })
+    setCourse(del)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='add-course text-center my-3'>
+        <input type='text' onChange={handleChange} placeholder='add your course' style={{fontFamily: "monospace", width: "300px", height: "38px", border: "2px solid #ccc", borderRadius: "7px"}}></input>
+        <button onClick={appendToList} className='btn btn-primary'>Add Course</button>
+        <div className='list my-3'>
+          {course.map((course, index)=> {
+            return (
+              <div>
+                <h1>{course}</h1>
+                <button className='btn btn-danger' onClick={()=> deleteCourse(course)}>x</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
